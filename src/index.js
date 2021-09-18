b3a732525e1665a816ab29fbcaf37e1602b30155
Hernan Condori <hcondori@elementum.com>
2021-09-18 14:26:22 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+console.log('REACT');
+
+const App = () => {
+  console.log('Soy el component APP');
+  return <p>Hola soy un component</p>;
+}
+
+const Titulo = () => {
+  const name = 'HOUSE';
+  console.log('Soy el component Titulo');
+
+  return (<h1>CODER {name} - CLASE 1</h1>)
+}
+
+const Description = () => <p>Esta funcion esta en sugar syntax</p>
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <App />
+      <Titulo />
+      <Description />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
